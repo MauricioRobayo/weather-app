025b68b148eb33ec831fb276f280987bf67d282f
Mauricio Robayo <rfmajo@gmail.com>
2019-11-13 15:12:20 -0500

    refactor: simplify entry point

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import WeatherApi from './js/weather-api'
 
 const WEATHER_API_KEY = 'fa1c7f10e6e75b7dc652e684202b5bd1'
 
-const session = new WeatherSession({
+new WeatherSession({
   parentElement: document.querySelector('main'),
   weatherApi: new WeatherApi(WEATHER_API_KEY),
-})
-
-session.startNewSession({ recursive: true })
+}).startNewSession()
