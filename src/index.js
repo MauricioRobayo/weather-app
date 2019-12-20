6b12e14763ff85e52c9fca7daab0d98bf08a3fa8
Mauricio Robayo <rfmajo@gmail.com>
2019-12-20 17:26:04 -0500

    style: add initial page loader

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import WeatherSession from './js/weather-session'
 import { fetchCity, fetchWeather } from './js/fetch-data'
+import { loader, element } from './js/helpers/elements-creators'
 
 const displayElement = document.querySelector('main')
+const pageLoader = element('div', {
+  textContent: 'Loading... ',
+  children: [loader()],
+})
+
+displayElement.append(pageLoader)
 
 fetchCity()
   .then(data => {
+    pageLoader.remove()
     const weatherSession = new WeatherSession({
       parentElement: displayElement,
       weatherApi: fetchWeather,
