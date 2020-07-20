08e6ae59fcaef3b0f7dcbfa60275d00accadde9f
Mauricio Robayo <rfmajo@gmail.com>
2020-07-20 11:59:36 -0500

    feat: use .env for local development

--- src/js/fetch-data.js
+++ src/js/fetch-data.js
@@ -1,3 +1,5 @@
+const PROXY_URL = 'https://vast-lake-71168.herokuapp.com/';
+
 const fromCache = (key, cacheInMinutes = 5) => {
   const cacheInMilliseconds = cacheInMinutes * 60 * 1000;
   if (localStorage[key] !== undefined) {
@@ -11,19 +13,18 @@ const fromCache = (key, cacheInMinutes = 5) => {
 };
 
 const fetchData = (endpoint, cacheInMinutes) => async (queryparams = {}) => {
-  const searchparams = new URLSearchParams(queryparams).toString();
-  const key = `${endpoint}-${searchparams}`;
+  const url = new URL(endpoint);
+  Object.entries(queryparams).forEach(([key, value]) => {
+    url.searchParams.append(key, value);
+  });
+  const key = `${url}`;
   const cache = fromCache(key, cacheInMinutes);
-
-  const url = ['localhost', '127.0.0.1'].includes(window.location.hostname)
-    ? 'http://localhost:5000'
-    : 'https://vast-lake-71168.herokuapp.com';
 
   if (cache) {
     return cache;
   }
 
-  const response = await fetch(`${url}/${endpoint}?${searchparams}`);
+  const response = await fetch(`${url}`);
   if (!response.ok) {
     throw new Error(`${response.statusText} (${response.status})`);
   }
@@ -32,7 +33,17 @@ const fetchData = (endpoint, cacheInMinutes) => async (queryparams = {}) => {
   return { ...data, cache: false };
 };
 
-const fetchCity = fetchData('ipinfo', 30);
-const fetchWeather = fetchData('weather', 10);
+const fetchCity = fetchData(
+  LOCAL // eslint-disable-line no-undef
+    ? `https://ipinfo.io/?token=${process.env.IPINFO_TOKEN}`
+    : `${PROXY_URL}ipinfo`,
+  30
+);
+const fetchWeather = fetchData(
+  LOCAL // eslint-disable-line no-undef
+    ? `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.WEATHER_API_KEY}`
+    : `${PROXY_URL}weathers`,
+  10
+);
 
 export { fetchCity, fetchWeather };
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,34 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.[contentHash].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-  },
-}
+module.exports = (env) => {
+  if (env.local) {
+    dotenv.config();
+  }
+
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'main.[contentHash].js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+      new webpack.DefinePlugin({
+        LOCAL: env.local,
+      }),
+      new webpack.EnvironmentPlugin({
+        IPINFO_TOKEN: '',
+        WEATHER_API_KEY: '',
+      }),
+    ],
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+    },
+    devtool: 'source-map',
+  };
+};
