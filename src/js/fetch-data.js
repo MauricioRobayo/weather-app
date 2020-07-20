45ef177030e5aef4645dd33afad2f17b646f2894
Mauricio Robayo <rfmajo@gmail.com>
2020-07-20 12:29:40 -0500

    feat: use simple-localstorage-package

--- src/js/fetch-data.js
+++ src/js/fetch-data.js
@@ -1,35 +1,31 @@
+import SLSC from 'simple-localstorage-cache';
+
 const PROXY_URL = 'https://vast-lake-71168.herokuapp.com/';
 
-const fromCache = (key, cacheInMinutes = 5) => {
-  const cacheInMilliseconds = cacheInMinutes * 60 * 1000;
-  if (localStorage[key] !== undefined) {
-    const cache = JSON.parse(localStorage[key]);
-    if (Date.now() - cache.datetime < cacheInMilliseconds) {
-      return { ...cache.data, cache: cache.datetime + cacheInMilliseconds };
-    }
-    localStorage.removeItem(key);
-  }
-  return false;
-};
-
-const fetchData = (endpoint, cacheInMinutes) => async (queryparams = {}) => {
+const fetchData = (endpoint, cacheInSeconds) => async (queryparams = {}) => {
   const url = new URL(endpoint);
+
   Object.entries(queryparams).forEach(([key, value]) => {
     url.searchParams.append(key, value);
   });
-  const key = `${url}`;
-  const cache = fromCache(key, cacheInMinutes);
 
-  if (cache) {
-    return cache;
+  const cache = new SLSC(url, cacheInSeconds);
+
+  if (cache.hasCache()) {
+    const { data, expiration } = cache.get();
+    return { ...data, cache: new Date(expiration) };
   }
 
-  const response = await fetch(`${url}`);
+  const response = await fetch(url);
+
   if (!response.ok) {
     throw new Error(`${response.statusText} (${response.status})`);
   }
+
   const data = await response.json();
-  localStorage[key] = JSON.stringify({ datetime: Date.now(), data });
+
+  cache.update(data);
+
   return { ...data, cache: false };
 };
 
@@ -37,13 +33,13 @@ const fetchCity = fetchData(
   LOCAL // eslint-disable-line no-undef
     ? `https://ipinfo.io/?token=${process.env.IPINFO_TOKEN}`
     : `${PROXY_URL}ipinfo`,
-  30
+  30 * 60
 );
 const fetchWeather = fetchData(
   LOCAL // eslint-disable-line no-undef
     ? `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.WEATHER_API_KEY}`
-    : `${PROXY_URL}weathers`,
-  10
+    : `${PROXY_URL}weather`,
+  10 * 60
 );
 
 export { fetchCity, fetchWeather };
