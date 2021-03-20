efb8c316b1ced6a1ab2cdc702c5d3613e854c9e3
Mauricio Robayo <rfmajo@gmail.com>
2021-03-20 01:52:23 -0500

    chore: update webpack config
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,35 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
-const dotenv = require('dotenv');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const dotenv = require("dotenv");
 
 module.exports = (env) => {
+  console.log(env.local, "🔥");
   if (env.local) {
     dotenv.config();
   }
 
   return {
-    entry: './src/index.js',
+    entry: "./src/index.js",
     output: {
-      filename: 'main.[contentHash].js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "main.[contentHash].js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './src/index.html',
+        template: "./src/index.html",
       }),
       new webpack.DefinePlugin({
         LOCAL: env.local,
       }),
       new webpack.EnvironmentPlugin({
-        IPINFO_TOKEN: '',
-        WEATHER_API_KEY: '',
+        IPINFO_TOKEN: "",
+        WEATHER_API_KEY: "",
       }),
     ],
     devServer: {
-      contentBase: path.join(__dirname, 'dist'),
+      contentBase: path.join(__dirname, "dist"),
     },
-    devtool: 'source-map',
+    devtool: "source-map",
   };
 };
